8936024c446f3c86ab18dea0306e82f4fdab44d4
Kateryna Tolstykh <agrratt@gmail.com>
2022-05-04 12:40:54 +0200

    fixed localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from 'nanoid';
 import { Form } from 'components/Form/Form';
 import { ListContacts } from 'components/ListContacts/ListContacts';
 import { Filter } from 'components/Filter/Filter';
 import { Container } from 'components/App.styled';
-import { useLocalStorage } from 'hooks/useLocalStorage';
+// import { useLocalStorage } from 'hooks/useLocalStorage';
 
 
 export function App() {
-  const [contacts, setContacts] = useLocalStorage([
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-  );
+  // const [contacts, setContacts] = useLocalStorage('contacts',
+  //   [
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ]
+  // );
+
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+
+  ]);
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const contacts = localStorage.getItem('KEY_LS');
+    const parsedContacts = JSON.parse(contacts);
+    
+    if (parsedContacts) {
+      setContacts(parsedContacts);
+    };
+
+  }, []);
+
+  useEffect(() => {
+    
+    localStorage.setItem('KEY_LS', JSON.stringify(contacts))
+    
+  }, [contacts]);
+   
 
   const addContacts = ({name, number}) => {
     const contact = {
